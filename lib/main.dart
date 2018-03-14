6573f2d24e5cc03997f94913d16c58b3daa9e622
shyshenok <shyshenoka@gmail.com>
2018-03-14 22:40:58 +0200

    Initial repository

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fs/pages/signIn/signIn.dart';
+
+void main() => runApp(
+    new MaterialApp(
+      title: "find()",
+        home: new SignIn()
+    ));
+
---
+++ lib/pages/signIn/singInBody.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class SingInBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Container(
+        color: Colors.blue,
+          child: new Center(
+              child:
+              new Text(
+                'Hello, worldттткккккт124!',
+              )
+          )
+      )
+
+    );
+  }
+}
