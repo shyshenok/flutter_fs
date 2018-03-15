d889cf3011aabd74e524475ad504879fbd862e72
shyshenok <shyshenoka@gmail.com>
2018-03-15 15:37:00 +0200

    Moderate icons constructor. Make it clickable.

--- lib/pages/signIn/singInBody.dart
+++ lib/pages/signIn/singInBody.dart
@@ -8,9 +8,7 @@ class SingInBody extends StatelessWidget {
       child: new Container(
         color: Colors.blue,
           child: new Center(
-              child: new Text(
-                'Hello, worldттткккккт124!',
-              )
+              child: new LoginIconsButton()
           )
       )
 
@@ -30,34 +28,45 @@ class _SelecteLogInAccount extends State<LoginIconsButton> {
         _ifClicked = !_ifClicked;
     });
   }
-
-  Row buildButtonColumn(IconData icon, String label) {
-    Color color = Theme.of(context).primaryColor;
-
-    return new Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        new Icon(icon, color: color),
-        new Container(
-          margin: const EdgeInsets.only(top: 8.0),
-          child: new Text(
-            label,
-            style: new TextStyle(
-              fontSize: 12.0,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).buttonColor;
+      Color clickedColor = Colors.red;
+
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          new IconButton(
+              icon: new Icon(icon),
+              color: (_ifClicked ? color : clickedColor),
+              onPressed: _clickedButton
+          ),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
     return new GestureDetector(
       onTap: _clickedButton,
+      child: new Container(
+        child: new Row(
+          children: [
+            buildButtonColumn(Icons.call, 'CALL',),
+          ]
+        ),
+      ),
     );
   }
 }
