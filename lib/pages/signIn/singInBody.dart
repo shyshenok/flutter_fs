4d5ff2b6aea67f0dc801fe1814376c1746ffa973
shyshenok <shyshenoka@gmail.com>
2018-03-15 16:13:20 +0200

    Change color also on te text

--- lib/pages/signIn/singInBody.dart
+++ lib/pages/signIn/singInBody.dart
@@ -5,18 +5,18 @@ class SingInBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Expanded(
-      child: new Container(
-        color: Colors.blue,
-          child: new Center(
-              child: new LoginIconsButton()
-          )
-      )
+        child: new Container(
+            color: Colors.blue,
+            child: new Center(
+                child: new LoginIconsButton()
+            )
+        )
 
     );
   }
 }
 
-class LoginIconsButton extends StatefulWidget{
+class LoginIconsButton extends StatefulWidget {
   _SelecteLogInAccount createState() => new _SelecteLogInAccount();
 }
 
@@ -25,45 +25,59 @@ class _SelecteLogInAccount extends State<LoginIconsButton> {
 
   void _clickedButton() {
     setState(() {
-        _ifClicked = !_ifClicked;
+      _ifClicked = !_ifClicked;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-
-    Row buildButtonColumn(IconData icon, String label) {
-      Color color = Theme.of(context).buttonColor;
+    GestureDetector buildButtonColumn(IconData icon, String label) {
+      Color color = Theme
+          .of(context)
+          .buttonColor;
       Color clickedColor = Colors.red;
-
-      return new Row(
-        children: [
-          new IconButton(
-              icon: new Icon(icon),
-              color: (_ifClicked ? color : clickedColor),
-              onPressed: _clickedButton
-          ),
-          new Container(
-            margin: const EdgeInsets.only(top: 8.0),
-            child: new Text(
-              label,
-              style: new TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w400,
-                color: color,
-              ),
+      return new GestureDetector(
+        onTap: _clickedButton,
+          child: new Container(
+            width: 200.0,
+            height: 200.0,
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                new IconButton(
+                    icon: new Icon(icon),
+                    color: (_ifClicked ? color : clickedColor),
+                    onPressed: _clickedButton
+                ),
+                new Container(
+                  margin: const EdgeInsets.only(top: 8.0),
+                  child: new Text(
+                    label,
+                    style: new TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w400,
+                      color:  _ifClicked ? color : clickedColor,
+                    ),
+                  ),
+//                  decoration: new BoxDecoration(
+//                    color: _ifClicked ? Colors.lightGreen[700] : Colors.grey[600],
+//                  ),
+                ),
+              ],
             ),
           ),
-        ],
+
       );
     }
-    return  new Container(
-        child: new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            buildButtonColumn(Icons.call, 'CALL',),
-          ]
-      ),
+    return new Container(
+          child: new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                buildButtonColumn(Icons.call, 'CALL',),
+              ]
+          ),
     );
   }
 }
