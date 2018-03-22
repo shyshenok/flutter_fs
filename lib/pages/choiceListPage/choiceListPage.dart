cf9515a7013a7dbe078481b8e96e2d101780cb03
shyshenok <shyshenoka@gmail.com>
2018-03-22 16:30:51 +0200

    Change item of side menu #6

--- lib/pages/choiceListPage/choiceListPage.dart
+++ lib/pages/choiceListPage/choiceListPage.dart
@@ -4,6 +4,11 @@ import 'package:flutter_fs/pages/signIn/signIn.dart';
 import 'package:flutter_fs/utils/usersManager.dart';
 
 
+const String _AccountName = 'Aravind Vemula';
+const String _AccountEmail = 'vemula.aravind336@gmail.com';
+const String _AccountAbbr = 'AV';
+
+
 class ChoiceList extends StatelessWidget {
 
 
@@ -12,36 +17,41 @@ class ChoiceList extends StatelessWidget {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text('Select list'),
-          actions: <Widget>[
-            new IconButton(
-                icon: new Icon (Icons.exit_to_app, color: Colors.white,),
-                onPressed: () {
-                  new UserManager().googleSignIn.signOut();
-                  Navigator.of(context).push(
-                    new PageRouteBuilder(
-                        pageBuilder: (_, __, ___) => new SignIn()
-                    ),
-                  );
-                }
-            )
-          ],
         ),
         drawer: new Drawer(
             child: new ListView(
-              children: <Widget> [
-                new DrawerHeader(child: new Text('Header'),),
+              children: <Widget>[
+                new UserAccountsDrawerHeader(
+                  accountName: const Text(_AccountName),
+                  accountEmail: const Text(_AccountEmail),
+                  currentAccountPicture: new CircleAvatar(
+                      backgroundColor: Colors.brown,
+                      child: new Text(_AccountAbbr)
+                  ),
+                ),
+
                 new ListTile(
-                  title: new Text('First Menu Item'),
+                  leading: new Icon(Icons.lightbulb_outline),
+                  title: new Text('Second Menu Item'),
                   onTap: () {},
                 ),
                 new ListTile(
+                  leading: new Icon(Icons.lightbulb_outline),
                   title: new Text('Second Menu Item'),
                   onTap: () {},
                 ),
                 new Divider(),
                 new ListTile(
-                  title: new Text('About'),
-                  onTap: () {},
+                  leading: new Icon(Icons.exit_to_app),
+                  title: new Text('Sign Out'),
+                  onTap: () {
+                    new UserManager().googleSignIn.signOut();
+                    Navigator.of(context).push(
+                      new PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => new SignIn()
+                      ),
+                    );
+                  },
                 ),
               ],
             )
