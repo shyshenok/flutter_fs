81c109c74455ea15429c84247281f4652b43c6ac
shyshenok <shyshenoka@gmail.com>
2018-03-27 17:45:43 +0300

    Adding text field and save button to modal window #4

--- lib/pages/choiceListPage/moviesList/moviesList.dart
+++ lib/pages/choiceListPage/moviesList/moviesList.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,23 +13,21 @@ class MoviesList extends StatelessWidget {
         child: new Stack(
           children: <Widget>[
             new Positioned (
+              right: 24.0,
+              bottom: 24.0,
               child: new Container(
                 child: new FloatingActionButton(
                     elevation: 6.0,
                     child: new Icon(Icons.add),
                     backgroundColor: new Color(0xFF228ba0),
                     onPressed: () {
-                      showDialog(context: context, child:
-                      new AlertDialog(
-                        title: new Text("My Super title"),
-                        content: new Text("Hello World"),
-                      )
+                      showDialog(
+                          context: context,
+                          child: new CreateMoviesList()
                       );
                     }
                 ),
               ),
-              right: 24.0,
-              bottom: 24.0,
             )
           ],
         ),
@@ -39,3 +36,44 @@ class MoviesList extends StatelessWidget {
   }
 }
 
+class CreateMoviesList extends StatefulWidget {
+  @override
+  State createState() => new CreateMoviesListState();
+}
+
+class CreateMoviesListState extends State<CreateMoviesList> {
+
+  final TextEditingController _listName = new TextEditingController();
+
+  void onPressed() {
+    print("button pressed $_listName");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new AlertDialog(
+      title: new Text("Create new list"),
+      content: new TextField(
+        autocorrect: true,
+        maxLength: 255,
+        maxLines: 1,
+        decoration: new InputDecoration(
+          hintText: 'Type list name',
+          labelText: 'List name'
+        ),
+        controller: _listName,
+        onChanged: (String text) {
+          if (_listName == null) {
+
+          }
+        },
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () {onPressed();},
+            child: new Text('Save')
+        ),
+      ],
+    );
+  }
+}
