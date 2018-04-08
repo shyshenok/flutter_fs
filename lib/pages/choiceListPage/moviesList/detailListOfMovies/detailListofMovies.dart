f1e8d7783426bf3f77788980cbef4a35a7b072f0
shyshenok <shyshenoka@gmail.com>
2018-04-08 20:25:28 +0300

    Create search #8

--- lib/pages/choiceListPage/moviesList/detailListOfMovies/detailListofMovies.dart
+++ lib/pages/choiceListPage/moviesList/detailListOfMovies/detailListofMovies.dart
@@ -1,14 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_fs/utils/listOfLists.dart';
 import 'package:meta/meta.dart';
-
+import 'package:material_search/material_search.dart';
 
 
 class DetailListOfMovies extends StatefulWidget {
 
   final ListOfLists data;
 
-  DetailListOfMovies({Key key, @required this.data}): super(key: key);
+  DetailListOfMovies({Key key, @required this.data}) : super(key: key);
 
   _DetailListOfMoviesState createState() => new _DetailListOfMoviesState(data);
 }
@@ -43,7 +45,7 @@ class _DetailListOfMoviesState extends State<DetailListOfMovies> {
                     child: new Icon(Icons.add),
                     backgroundColor: new Color(0xFF228ba0),
                     onPressed: () {
-
+                      _openAddEntryDialog();
                     }
                 ),
               ),
@@ -52,6 +54,75 @@ class _DetailListOfMoviesState extends State<DetailListOfMovies> {
         ),
       ),
     );
+  }
+
+  void _openAddEntryDialog() {
+    Navigator.of(context).push(new MaterialPageRoute<Null>(
+        builder: (BuildContext context) {
+          return new AddEntryDialog();
+        },
+        fullscreenDialog: true
+    ));
+  }
+
+
+}
+
+class AddEntryDialog extends StatefulWidget {
+  @override
+  AddEntryDialogState createState() => new AddEntryDialogState();
+}
+
+class AddEntryDialogState extends State<AddEntryDialog> {
+
+  TextEditingController _movieName = new TextEditingController();
+
+  final _names = [
+    'Igor Minar',
+    'Brad Green',
+    'Dave Geddes',
+    'Naomi Black',
+    'Greg Weber',
+    'Dean Sofer',
+    'Wes Alvaro',
+    'John Scott',
+    'Daniel Nadasi',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      body: new MaterialSearch<String>(
+        //placeholder of the input and of the search bar text input
+        placeholder: 'Search',
+        limit: 100,
+        getResults: (String criteria) async {
+          var list = await _fetchList(criteria);
+          return list.map((name) =>
+          new MaterialSearchResult<String>(
+            value: name, //The value must be of type <String>
+            text: name, //String that will be show in the list
+            icon: Icons.person,
+          )).toList();
+        },
+        filter: (String value, String criteria) {
+          return value.toString().toLowerCase().trim()
+              .contains(new RegExp(r'' + criteria.toLowerCase().trim() + ''));
+        },
+        onSelect: (String selected) {
+          if (selected == null) {
+            return;
+          }
+
+          print("Selected: " + selected);
+        },
+      ),
+    );
+  }
+
+  Future<List<String>> _fetchList(String criteria) {
+    return new Future.value(_names);
   }
 
 
